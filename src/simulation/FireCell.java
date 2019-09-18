ab267bd6fedc40a127a345b53716086acbe3cf24
Erie Seong Ho Han <eh174@duke.edu>
2019-09-18 02:22:11 -0400

    Code for Fire, Segregation added

--- src/simulation/FireCell.java
+++ src/simulation/FireCell.java
@@ -6,23 +6,37 @@ import javafx.scene.shape.Rectangle;
 public class FireCell extends Cell {
 
     private double probCatch;
+    private boolean fireNearby;
 
     public FireCell(int row, int col, int state, double probCatch) {
         super(row, col, state);
-
         this.probCatch = probCatch;
     }
+
     @Override
     public void check() {
+        //if already burnt or in fire, will be empty at next round.
+        if(getState()==0) {setNextState(0);}
+        else if(getState()==1) {setNextState(0);}
 
+        else {
+            for (Cell neighbor : getEdgeNeighbor()) {
+                if (neighbor.getState()==1) {
+                    fireNearby = true;
+                    return;
+                }
+            }
+            if(fireNearby) {
+                if(Math.random() < probCatch) {setNextState(1);}
+            } else {
+                setNextState(2);
+            }
+        }
     }
 
     @Override
     public void changeState() {
-
-    }
-
-    private Paint stateToPaint() {
-        return null;
+        fireNearby = false;
+        setState(getNextState());
     }
 }
