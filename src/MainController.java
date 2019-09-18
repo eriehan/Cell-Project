4591ee1538c1e62e6c79475c1ede6382b4c95222
dl257 <dl257@duke.edu>
2019-09-18 00:40:23 -0400

    xml parsing started

--- src/MainController.java
+++ src/MainController.java
@@ -7,12 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import userInterface.UserInterface;
 import userInterface.VisualizationConstants;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -27,6 +27,7 @@ public class MainController extends Application {
     private Scene myScene;
     private Stage myStage;
     private Animation myAnimation;
+    private String myTitle = "Change Me";
     private int updateTimer; // TODO: backend can use this to control speed of update
 
 
@@ -34,7 +35,7 @@ public class MainController extends Application {
     public void start(Stage stage) throws Exception {
         updateTimer =0;
 
-        myUserInterface = new UserInterface(100, 100, "Some Simulation");
+        myUserInterface = new UserInterface(100, 100, myTitle);
         myUserInterface.getMyGridView().createGrid();
         myScene = initScene();
         stage.setScene(myScene);
@@ -64,23 +65,42 @@ public class MainController extends Application {
 
     private Scene initScene() throws IOException, SAXException, ParserConfigurationException {
         Group root = myUserInterface.setScene();
-       // parseXML();
+        parseXML();
         var scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT, BACKGROUND_COLOR);
         return scene;
     }
 
     private void parseXML() throws ParserConfigurationException, IOException, SAXException {
-//        //TODO: parseXML code
-//        File xmlFile = new File("simulation1.xml");
-//        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder docBuilder = documentBuilderFactory.newDocumentBuilder();
-//        Document doc = docBuilder.parse(xmlFile);
-//        NodeList typeOfSimulation = doc.getElementsByTagName("Game");
-//        for(int i = 0; i < typeOfSimulation.getLength(); i++){
-//            if(typeOfSimulation.item(i).getAttributes().equals("Game Of Life")){
-//                System.out.println("game of Life");
-//            }
-//        }
+        //TODO: parseXML code
+        File xmlFile = new File("xml_files/simulation1.xml");
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(xmlFile);
+
+        NodeList typeOfSimulation = doc.getElementsByTagName("Game");
+
+        for(int i = 0; i < typeOfSimulation.getLength(); i++){
+            Node currentGameType = typeOfSimulation.item(i);
+
+            if(currentGameType.getAttributes().item(i).getNodeValue().equals("Game Of Life")) {
+                if(currentGameType.getNodeType() == Node.ELEMENT_NODE){
+                    Element currElement = (Element)currentGameType;
+                    GRID_WIDTH = Integer.parseInt(currElement.getElementsByTagName("Width").item(0).getTextContent());
+                    GRID_HEIGHT = Integer.parseInt(currElement.getElementsByTagName("Height").item(0).getTextContent());
+                    NodeList listOfCoord = currElement.getElementsByTagName("Cells");
+
+                    //returns the x and y
+                    System.out.println(listOfCoord.item(0));
+
+
+
+
+                }
+
+
+
+            }
+        }
 
     }
 
