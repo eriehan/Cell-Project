a8e4a5c1d04bd9565013fdb64b3ed8e0dfd58226
Diane Lin <diane.lin@duke.edu>
2019-09-26 15:29:25 -0400

    refactored xml object into xml abstract class

---
+++ src/xml/PercolationXml.java
@@ -0,0 +1,50 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import userInterface.AbstractGridView;
+import userInterface.UserInterface;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class PercolationXml extends AbstractXml {
+    public PercolationXml(UserInterface myUserInterface){
+        super(myUserInterface);
+    }
+
+    @Override
+    protected void setUpSimulationParameters(ArrayList<ArrayList<Integer>> myColArray, ArrayList<ArrayList<Integer>> myRowArray, Document doc) {
+        this.cellGridColNum = Integer.parseInt(doc.getElementsByTagName("Col").item(0).getTextContent());
+        this.myUserInterface.setNumOfCols(this.cellGridColNum);
+        this.cellGridRowNum = Integer.parseInt(doc.getElementsByTagName("Row").item(0).getTextContent());
+        this.myUserInterface.setNumOfRows(this.cellGridRowNum);
+        this.rate = Integer.parseInt(doc.getElementsByTagName("Rate").item(0).getTextContent());
+        this.isSaved = Integer.parseInt(doc.getElementsByTagName("Saved").item(0).getTextContent());
+        NodeList typeOfSimulation = doc.getElementsByTagName("Type");
+        //goes through NodeList to get information about the different simulation
+        for (int i = 0; i < typeOfSimulation.getLength(); i++) {
+            //gets the i-th simulation type and casts it as a node
+            Node currentSimulationType = typeOfSimulation.item(i);
+            Element currentSimulationElement = (Element) currentSimulationType;
+            this.myTitle = currentSimulationElement.getAttribute("name");
+            this.myUserInterface.changeTitle(this.myTitle);
+            this.numAgents = Integer.parseInt(currentSimulationElement.getTextContent());
+        }
+        for (int i = 0; i < this.numAgents; i++) {
+            NodeList agent = doc.getElementsByTagName("Agent" + i);
+            Element n = (Element) agent.item(0);
+            String row = n.getElementsByTagName("Row").item(0).getTextContent();
+            String col = n.getElementsByTagName("Column").item(0).getTextContent();
+            myRowArray.add(stringToIntArray(row));
+            myColArray.add(stringToIntArray(col));
+        }
+    }
+
+    @Override
+    public void saveCurrentSimulation(AbstractGridView myGridView, File xmlFilePath) {
+
+    }
+}
---
+++ src/xml/WaTorXml.java
@@ -0,0 +1,69 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import userInterface.AbstractGridView;
+import userInterface.UserInterface;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class WaTorXml extends AbstractXml{
+    private ArrayList<Integer> EnergyArray = new ArrayList<>();
+    private ArrayList<Integer> MaturityArray = new ArrayList<>();
+
+    public WaTorXml(UserInterface myUserInterFace){
+        super(myUserInterFace);
+    }
+
+    @Override
+    protected void setUpSimulationParameters(ArrayList<ArrayList<Integer>> myColArray, ArrayList<ArrayList<Integer>> myRowArray, Document doc) {
+        this.cellGridColNum = Integer.parseInt(doc.getElementsByTagName("Col").item(0).getTextContent());
+        this.myUserInterface.setNumOfCols(this.cellGridColNum);
+        this.cellGridRowNum = Integer.parseInt(doc.getElementsByTagName("Row").item(0).getTextContent());
+        this.myUserInterface.setNumOfRows(this.cellGridRowNum);
+        this.rate = Integer.parseInt(doc.getElementsByTagName("Rate").item(0).getTextContent());
+        this.isSaved = Integer.parseInt(doc.getElementsByTagName("Saved").item(0).getTextContent());
+        NodeList typeOfSimulation = doc.getElementsByTagName("Type");
+        //goes through NodeList to get information about the different simulation
+        for (int i = 0; i < typeOfSimulation.getLength(); i++) {
+            //gets the i-th simulation type and casts it as a node
+            Node currentSimulationType = typeOfSimulation.item(i);
+            Element currentSimulationElement = (Element) currentSimulationType;
+            this.myTitle = currentSimulationElement.getAttribute("name");
+            this.myUserInterface.changeTitle(this.myTitle);
+            this.numAgents = Integer.parseInt(currentSimulationElement.getTextContent());
+        }
+        for (int i = 0; i < this.numAgents; i++) {
+            NodeList agent = doc.getElementsByTagName("Agent" + i);
+            Element n = (Element) agent.item(0);
+            this.EnergyArray.add(Integer.parseInt(n.getElementsByTagName("Energy").item(0).getTextContent()));
+            this.MaturityArray.add(Integer.parseInt(n.getElementsByTagName("Mature").item(0).getTextContent()));
+            String row = n.getElementsByTagName("Row").item(0).getTextContent();
+            String col = n.getElementsByTagName("Column").item(0).getTextContent();
+            myRowArray.add(stringToIntArray(row));
+            myColArray.add(stringToIntArray(col));
+        }
+
+    }
+
+    @Override
+    public void saveCurrentSimulation(AbstractGridView myGridView, File xmlFilePath) {
+
+    }
+
+
+    public ArrayList<Integer> getEnergyArray(){
+        ArrayList<Integer> copy = new ArrayList<>();
+        copy.addAll(EnergyArray);
+        return copy;
+    }
+
+    public ArrayList<Integer> getMaturityArray(){
+        ArrayList<Integer> copy = new ArrayList<>();
+        copy.addAll(MaturityArray);
+        return copy;
+    }
+}
