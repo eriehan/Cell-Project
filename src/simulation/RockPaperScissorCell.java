194534d5c6e34727be23e69e6c2302e42798698d
Erie Seong Ho Han <eh174@duke.edu>
2019-09-26 10:26:09 -0400

    RockPaperScissor classes added

---
+++ src/simulation/RockPaperScissorCell.java
@@ -0,0 +1,18 @@
+package simulation;
+
+public class RockPaperScissorCell extends Cell {
+
+    public RockPaperScissorCell(int row, int col, CellState state) {
+        super(row, col, state);
+    }
+
+    @Override
+    public void check() {
+
+    }
+
+    @Override
+    public void changeState() {
+
+    }
+}
